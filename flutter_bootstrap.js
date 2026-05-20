8f6489cfb96a417af11e0885215aa22cb19e11c3
cheGv <cheGv@users.noreply.github.com>
2026-05-20 14:23:36 +0000

    deploy: 8daa323af6fc72385ee763a4d0a1b15fd6946702

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3084181867" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2869580803" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
